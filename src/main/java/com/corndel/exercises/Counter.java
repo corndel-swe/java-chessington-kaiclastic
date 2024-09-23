f7441663db8690c73751f674338604b9176f57ef
kaiclastic <halajash567@outlook.com>
2024-09-23 11:18:24 +0100

    solves excerise 2

--- src/main/java/com/corndel/exercises/Counter.java
+++ src/main/java/com/corndel/exercises/Counter.java
@@ -1,10 +1,23 @@
 package com.corndel.exercises;
 
 public class Counter {
+    // Define instance variable
     int count ;
-
+    // Define constructor variable
         public Counter() {
         this.count = 0;
 
+    }
+    // Define a method
+    // increment(), which adds one to count
+    public void increment() {
+        this.count ++;
+    }
+
+    public void reset() {
+        this.count = 0;
+    }
+    public int getCount() {
+       return this.count;
     }
 }
